d4ecfdd0a032d32033bbc7d879ff2b1d385234ae
rootrsk <ravishankar7050@gmail.com>
2020-08-27 02:07:40 +0530

    Updates

---
+++ precache-manifest.afedfc31cb89b4a9f92d388631361591.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569bb69d3f79c7fac55fc294ab1e5a8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8537275b091ebed95144",
+    "url": "/portfolio/static/css/main.f38689b1.chunk.css"
+  },
+  {
+    "revision": "79bd691a8cd2ca16bcbb",
+    "url": "/portfolio/static/js/2.5d9d7fda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.5d9d7fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8537275b091ebed95144",
+    "url": "/portfolio/static/js/main.a1e823d6.chunk.js"
+  },
+  {
+    "revision": "af576cafede080114400",
+    "url": "/portfolio/static/js/runtime-main.38e2fad2.js"
+  },
+  {
+    "revision": "980ef57fb9d73f29dbf205fc6b1aacfb",
+    "url": "/portfolio/static/media/Node.980ef57f.png"
+  },
+  {
+    "revision": "6d11f024003dd091cdba159dcaff1ca7",
+    "url": "/portfolio/static/media/css.6d11f024.png"
+  },
+  {
+    "revision": "1443fe76a52e2f31abd4711774228822",
+    "url": "/portfolio/static/media/express.1443fe76.png"
+  },
+  {
+    "revision": "5695b879efa92a40246cb010532a947f",
+    "url": "/portfolio/static/media/html.5695b879.png"
+  },
+  {
+    "revision": "3b92634f45c3d35efc55369ff2a03576",
+    "url": "/portfolio/static/media/img4.3b92634f.png"
+  },
+  {
+    "revision": "34c325ff3b99a46b61f55a1a0c04967f",
+    "url": "/portfolio/static/media/javascript.34c325ff.png"
+  },
+  {
+    "revision": "f0f9f104169bf473b311a55940e22116",
+    "url": "/portfolio/static/media/profile.f0f9f104.jpg"
+  },
+  {
+    "revision": "37728de5836e206796a35da671696494",
+    "url": "/portfolio/static/media/reactLo.37728de5.png"
+  },
+  {
+    "revision": "1a947147e7fbdb735ef1343208e0f028",
+    "url": "/portfolio/static/media/socket.1a947147.png"
+  }
+]);
